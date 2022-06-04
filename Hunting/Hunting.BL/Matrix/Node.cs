81003483f88811450095fb48924d2fc156e87de2
Zaika Bogdan <zaika.bogdan2001@gmail.com>
2022-06-04 14:43:48 +0300

    Queue

--- Hunting/Hunting.BL/Matrix/Node.cs
+++ Hunting/Hunting.BL/Matrix/Node.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 namespace Hunting.BL.Matrix;
 
-public class Node
+public class Node : IEquatable<Node>
 {
     public Unit? Unit { get; set; }
     public Surface Surface { get; set; }
@@ -13,4 +13,24 @@ public class Node
     public int Y { get; set; }
     [JsonIgnore]
     internal int TurnsAfterGrassEating { get; set; }
+
+    public bool Equals(Node? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Node)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return (X * 397) ^ Y;
+    }
 }
